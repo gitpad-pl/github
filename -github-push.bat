ccbc940dbcdedb7334b072538df3d6949f530898
tom <tom@sapletta.com>
2019-05-22 23:04:16 +0200

    test7

--- -github-push.bat
+++ -github-push.bat
@@ -1,3 +1,5 @@
 :: update data on git repo
 SetLocal EnableDelayedExpansion
--message.bat & git add . & git commit -m "%message%" && git push origin master && start firefox "https://github.com/gitpad-pl/github.git"
+::-message.bat
+for /f %%i in ('dir /b /od Ticket\*.md') do set filename=%%i
+for /f "delims==" %%a in (Ticket\%filename%) do set message=%%a && git add . && git commit -m "%message%" && git push origin master && start firefox "https://github.com/gitpad-pl/github.git"
