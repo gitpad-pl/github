12c16dc0c57e34b3342b1b0c8b25dae2894a7ca3
tom <tom@sapletta.com>
2019-05-22 23:04:57 +0200

    test90

--- -github-push.bat
+++ -github-push.bat
@@ -2,4 +2,5 @@
 SetLocal EnableDelayedExpansion
 ::-message.bat
 for /f %%i in ('dir /b /od Ticket\*.md') do set filename=%%i
-for /f "delims==" %%a in (Ticket\%filename%) do set message=%%a && git add . && git commit -m "%message%" && git push origin master && start firefox "https://github.com/gitpad-pl/github.git"
+for /f "delims==" %%a in (Ticket\%filename%) do set message=%%a
+git add . && git commit -m "%message%" && git push origin master && start firefox "https://github.com/gitpad-pl/github.git"
