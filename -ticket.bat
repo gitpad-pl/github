0ed4fcc74dcc3e346d826fc33a5f00dde392b7ca
tom <tom@sapletta.com>
2019-05-22 18:44:58 +0200

    Creating New format for Tickets, as Version

--- -ticket.bat
+++ -ticket.bat
@@ -1,3 +1,5 @@
 @ECHO OFF
 for /f %%i in ('dir /b /od Ticket\*.md') do @set filename=%%i
-set /p ticket=<Ticket\%filename%
+::set /p ticket=<Ticket\%filename%
+for /f "delims==" %%a in (Ticket\%filename%) do set ticket=%%a
+::echo %ticket%
