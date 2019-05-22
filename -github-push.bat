bf6904c6d6431ca67b8e93d9512a54a7b75bd403
tom <tom@sapletta.com>
2019-05-22 22:55:31 +0200

    test4

--- -github-push.bat
+++ -github-push.bat
@@ -1,2 +1,2 @@
 :: update data on git repo
--message.bat && git add . && git commit -m "%message%" && git push origin master && start firefox "https://github.com/gitpad-pl/github.git"
+-message.bat & git add . & git commit -m "%message%" && git push origin master && start firefox "https://github.com/gitpad-pl/github.git"
