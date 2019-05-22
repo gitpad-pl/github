23380785bef728216857c5536e5054c74a48ed9b
tom <tom@sapletta.com>
2019-05-22 18:26:41 +0200

    Create solution for automatically commit and push with message as content last used ticket file

--- g-push.bat
+++ g-push.bat
@@ -1,3 +1,2 @@
 :: update data on git repo
-g-ticket.bat && git add . && git commit -m "%ticket%" && git push origin master
-start firefox "https://github.com/gitpad-pl/github.git"
+g-ticket.bat && git add . && git commit -m "%ticket%" && git push origin master && start firefox "https://github.com/gitpad-pl/github.git"
