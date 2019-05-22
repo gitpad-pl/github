edeb6dd5222eb2ec2cdb20e63403e03de771c6ca
tom <tom@sapletta.com>
2019-05-22 23:47:22 +0200

    New Version of System v1.0.24

--- -npm-publish.bat
+++ -npm-publish.bat
@@ -1,3 +1,3 @@
 :: publish npm package on npmjs.com
 ::npm version patch &&
-npm publish --access public && start firefox "https://www.npmjs.com/package/gitpad-github" && SLEEP 9 && -version.bat
+npm publish --access public && start firefox "https://www.npmjs.com/package/gitpad-github" && pause && -version.bat
