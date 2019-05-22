cd591f82810bf2aa568e70c0eb04508d06d0e29a
tom <tom@sapletta.com>
2019-05-22 23:39:04 +0200

    make -version after -npm-publish

--- -npm-publish.bat
+++ -npm-publish.bat
@@ -1,3 +1,3 @@
 :: publish npm package on npmjs.com
 ::npm version patch &&
-npm publish --access public && start firefox "https://www.npmjs.com/package/gitpad-github"
+npm publish --access public && start firefox "https://www.npmjs.com/package/gitpad-github" && -version.bat
