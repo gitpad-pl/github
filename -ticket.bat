a15153963db6df2e2abcdae8aa03ca653f4e26d2
tom <tom@sapletta.com>
2019-05-22 19:02:07 +0200

    Update Documentation

--- -ticket.bat
+++ -ticket.bat
@@ -2,4 +2,4 @@
 for /f %%i in ('dir /b /od Ticket\*.md') do @set filename=%%i
 ::set /p ticket=<Ticket\%filename%
 for /f "delims==" %%a in (Ticket\%filename%) do set ticket=%%a
-::echo %ticket%
+echo %ticket%
