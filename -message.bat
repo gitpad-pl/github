1516953ee3a5a9ceeffa83395db1b8fbb69baf61
tom <tom@sapletta.com>
2019-05-22 22:52:53 +0200

    New Version of System v1.0.20

--- -message.bat
+++ -message.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
-for /f %%i in ('dir /b /od Ticket\*.md') do set /p filename=%%i
+for /f %%i in ('dir /b /od Ticket\*.md') do set filename=%%i
 ::set /p ticket=<Ticket\%filename%
 :: sed 's/\r$//' Ticket\%filename% > Ticket\%filename%
 ::TYPE Ticket\%filename% | MORE /P > Ticket\%filename%
 ::echo %filename% &&
-for /f "delims==" %%a in (Ticket\%filename%) do set /p message=%%a
+for /f "delims==" %%a in (Ticket\%filename%) do @set message=%%a
 echo %message%
