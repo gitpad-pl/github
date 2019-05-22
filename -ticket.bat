1e1c22b8da386b941d96d829fa7de4c8ee723250
tom <tom@sapletta.com>
2019-05-22 22:06:54 +0200

    New Version of System v1.0.14

--- -ticket.bat
+++ -ticket.bat
@@ -5,7 +5,7 @@
 set CONTENT=%~1
 ::echo %CONTENT%
 set /p VERSION= < VERSION.txt
-IF "%CONTENT%" == "" set CONTENT="New Version of System v%VERSION%"
+IF "%CONTENT%" == "" set CONTENT=New Version of System v%VERSION%
 echo %CONTENT% >> "Ticket\v%VERSION%.md"
 ::echo %CONTENT%
 ::node -pe "require('./package.json').version" > VERSION.txt &&
