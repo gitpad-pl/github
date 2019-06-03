a2d10a1be17c9003f5edd61045b39cbca14c89c2
tom <tom@sapletta.com>
2019-06-03 14:54:33 +0200

    New Version of System v1.0.64

--- -install-package.bat
+++ -install-package.bat
@@ -1,5 +1,8 @@
 :: Install the dependencies
 :: npm cache clean
 npm install axios
+npm install gitpad-decode
+npm install gitpad-empty
 npm install gitpad-each
+npm install gitpad-each-filter
 npm i -g package-json-to-readme
